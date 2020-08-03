6eb8e456c2e1ca7826edf1e53ced00cdb9b63807
Jan Dusek <j@koiru.com>
2020-08-03 11:49:43 +0800

    Fixed font in MsgComposer TextArea

--- src/components/MsgComposer.js
+++ src/components/MsgComposer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled, { keyframes, css } from 'styled-components';
-import chroma from "chroma-js";
+import chroma from 'chroma-js';
 
 const textAreaMaxHeight = 82; // px
 //const msgOutboundUrl = 'https://build-client-backend-5498-dev.twil.io/msgOutbound';
@@ -10,12 +10,12 @@ const hover_accent = chroma(process.env.REACT_APP_ACCENT_COLOR).darken().hex();
 
 export default class MsgComposer extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      msgText: "",
+      msgText: '',
       sending: false,
       error: null
-    }
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.handleSend = this.handleSend.bind(this);
@@ -25,7 +25,10 @@ export default class MsgComposer extends Component {
 
   resizeTextArea(textArea) {
     textArea.style.height = 'inherit';
-    textArea.style.height = `${Math.min(textArea.scrollHeight, textAreaMaxHeight)}px`;
+    textArea.style.height = `${Math.min(
+      textArea.scrollHeight,
+      textAreaMaxHeight
+    )}px`;
   }
 
   handleChange(e) {
@@ -34,7 +37,8 @@ export default class MsgComposer extends Component {
   }
 
   handleKeyDown(e) {
-    if (e.keyCode === 13) {  // Enter
+    if (e.keyCode === 13) {
+      // Enter
       if (e.shiftKey) {
         this.resizeTextArea(e.target);
       } else {
@@ -52,14 +56,18 @@ export default class MsgComposer extends Component {
   handleSend() {
     this.setState({ sending: true });
     function formParams(params) {
-      return Object.keys(params).map((key) => {
-        return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
-      }).join('&');
+      return Object.keys(params)
+        .map((key) => {
+          return (
+            encodeURIComponent(key) + '=' + encodeURIComponent(params[key])
+          );
+        })
+        .join('&');
     }
 
     const contact = this.props.selectedContact;
     let toNumber = contact;
-    if (contact === "new") {
+    if (contact === 'new') {
       toNumber = this.normalizePhoneNumber(this.props.newPhoneNumber);
     }
     const body = formParams({
@@ -70,15 +78,15 @@ export default class MsgComposer extends Component {
     fetch(msgOutboundUrl, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/x-www-form-urlencoded'
       },
       body
     })
-      .then(async response => {
+      .then(async (response) => {
         if (response.status === 200) {
           this.setState({ msgText: '', sending: false, error: null });
-          if (contact === "new") {
-            this.props.updateNewPhoneNumber("");
+          if (contact === 'new') {
+            this.props.updateNewPhoneNumber('');
             this.props.selectContact(toNumber);
           }
           this.resizeTextArea(this.msgTextRef.current);
@@ -87,15 +95,20 @@ export default class MsgComposer extends Component {
           console.error(response.status, errText);
           this.setState({ sending: false, error: errText });
         }
-      }).catch(err => {
-        this.setState({ sending: false, error: ("Backend err:" + err) });
-        console.error("Error while sending message:", err)
+      })
+      .catch((err) => {
+        this.setState({ sending: false, error: 'Backend err:' + err });
+        console.error('Error while sending message:', err);
       });
   }
 
   render() {
     return [
-      (this.state.error ? <Error key="error"><ErrorText>{this.state.error}</ErrorText></Error> : null),
+      this.state.error ? (
+        <Error key="error">
+          <ErrorText>{this.state.error}</ErrorText>
+        </Error>
+      ) : null,
       <Container key="msgComposer">
         <TextAreaContainer key="txtAreaContainer">
           <TextArea
@@ -108,8 +121,9 @@ export default class MsgComposer extends Component {
             disabled={this.state.sending}
           />
         </TextAreaContainer>
-        <BtnSend key="btnSend"
-          disabled={this.state.msgText === "" || this.state.sending}
+        <BtnSend
+          key="btnSend"
+          disabled={this.state.msgText === '' || this.state.sending}
           sending={this.state.sending}
           onClick={this.handleSend}
         >
@@ -129,7 +143,7 @@ const Container = styled.div`
   color: rgb(34, 34, 34);
   position: relative;
   overflow-x: hidden;
-  
+
   display: flex;
   flex-wrap: nowrap;
   -webkit-box-flex: 0;
@@ -168,7 +182,10 @@ const TextArea = styled.textarea`
   padding-bottom: 6px;
   overflow-x: hidden;
   overflow-y: auto;
-  color: ${props => (props.disabled ? "#94979B" : "rgb(34, 34, 34)")};
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  color: ${(props) => (props.disabled ? '#94979B' : 'rgb(34, 34, 34)')};
   border-width: 0px;
   border-style: initial;
   border-color: initial;
@@ -199,23 +216,29 @@ const BtnSend = styled.button`
   border-radius: 100px;
   background: ${process.env.REACT_APP_ACCENT_COLOR};
   margin-left: 8px;
-  ${(props) => props.disabled ?
-    `
+  ${(props) =>
+    props.disabled
+      ? `
     opacity: 0.5;
     cursor: default;
-    ` : `
+    `
+      : `
     opacity: 1;
     cursor: pointer;
     &:hover { background: ${hover_accent}; }
-    `
-  }
-  animation: ${props => (props.sending ? css`${flash} 1s ease-in-out infinite` : '')};
+    `}
+  animation: ${(props) =>
+    props.sending
+      ? css`
+          ${flash} 1s ease-in-out infinite
+        `
+      : ''};
 `;
 
 const Error = styled.div`
   text-align: center;
-  padding:2px;
-  margin:0;
+  padding: 2px;
+  margin: 0;
 `;
 
 const ErrorText = styled.div`
@@ -228,15 +251,17 @@ const ErrorText = styled.div`
   font-weight: 400;
 `;
 
-const IcoSend = <svg
-  width="24px"
-  height="24px"
-  className="Twilio-Icon-Content"
-  viewBox="0 0 24 24"
->
-  <path
-    fill="currentColor"
-    fillRule="evenodd"
-    d="M19.641 4c.164 0 .27.064.317.193.047.13.023.305-.07.528l-7.19 16.77a.761.761 0 01-.237.36.501.501 0 01-.308.114.494.494 0 01-.281-.097.989.989 0 01-.264-.29L8.76 16.92a4.609 4.609 0 00-.73-.905 5.269 5.269 0 00-.904-.747L2.38 12.314c-.27-.175-.396-.372-.378-.588.017-.217.172-.39.465-.519l16.788-7.12c.07-.023.137-.043.202-.06A.701.701 0 0119.64 4zM7.723 14.266c.117.07.238.155.36.255.124.1.25.208.379.325l8.314-8.455-12.902 5.484 3.85 2.39zm4.342 5.818l5.52-12.92-8.332 8.473c.105.117.202.234.29.351.088.117.16.235.22.352l2.302 3.744z"
-  ></path>
-</svg>;
+const IcoSend = (
+  <svg
+    width="24px"
+    height="24px"
+    className="Twilio-Icon-Content"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M19.641 4c.164 0 .27.064.317.193.047.13.023.305-.07.528l-7.19 16.77a.761.761 0 01-.237.36.501.501 0 01-.308.114.494.494 0 01-.281-.097.989.989 0 01-.264-.29L8.76 16.92a4.609 4.609 0 00-.73-.905 5.269 5.269 0 00-.904-.747L2.38 12.314c-.27-.175-.396-.372-.378-.588.017-.217.172-.39.465-.519l16.788-7.12c.07-.023.137-.043.202-.06A.701.701 0 0119.64 4zM7.723 14.266c.117.07.238.155.36.255.124.1.25.208.379.325l8.314-8.455-12.902 5.484 3.85 2.39zm4.342 5.818l5.52-12.92-8.332 8.473c.105.117.202.234.29.351.088.117.16.235.22.352l2.302 3.744z"
+    ></path>
+  </svg>
+);
