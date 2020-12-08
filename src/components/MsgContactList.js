e99af9fb091c17b177489893a2d5351db59df6f6
Jan Dusek <j@koiru.com>
2020-12-08 15:46:22 +0800

    Addressing minor warning

--- src/components/MsgContactList.js
+++ src/components/MsgContactList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { printTimestamp, formatBodyPreviewText } from '../helpers.js';
 import { parsePhoneNumberFromString } from 'libphonenumber-js';
